dde26858a38027777c5d72e9019905ee9d543f1c
andreeacristiana13 <118850608+andreeacristiana13@users.noreply.github.com>
2024-04-24 22:12:33 +0300

    created login for admin (#6)
    Added authorized controller and fixed dbContext

--- IFAB/Migrations/20240424181645_Initial.Designer.cs
+++ IFAB/Migrations/20240424181645_Initial.Designer.cs
@@ -4,6 +4,7 @@ using IFAB.AppDbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IFAB.Migrations
 {
     [DbContext(typeof(IFABDbContext))]
-    partial class IFABDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240424181645_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
