c08c7e1cf79e369955a1d57f8b963da8786aac4d
BolduDenis <denisboldu2014@gmail.com>
2024-05-08 21:35:02 +0300

    Feature quiz added

---
+++ IFAB/Controllers/QuizController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IFAB.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult Quiz()
+        {
+            return View();
+        }
+    }
+}
